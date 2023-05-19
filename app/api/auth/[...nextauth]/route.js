2f229ed586e3bbaeda1b1ab1bb8de2f4e906be1c
veekta <vsunday.1710@gmail.com>
2023-05-19 07:59:12 +0100

    oauth debug

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,7 +1,5 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
-import { handleAuth } from "@auth0/nextjs-auth0";
-
 import User from "@models/user";
 import { connectToDB } from "@utils/database";
 
